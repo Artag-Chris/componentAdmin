7c4a865d8f9f4275e8698a0c2a2f3eebab4a37cb
Artag-Chris <scristxyz@gmail.com>
2024-09-06 16:37:17 -0500

    terminando de configurar tailwind

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,20 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import tailwindcss from 'eslint-plugin-tailwindcss';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    ignores: ['dist']
+  },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended
+    ],
+    files: ['**/*.ts', '**/*.tsx'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -16,6 +22,7 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'tailwindcss': tailwindcss,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +30,8 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'tailwindcss/classnames-order': 'warn',
+      'tailwindcss/no-custom-classname': 'off', // Puedes ajustar esta regla según tus necesidades
     },
-  },
-)
+  }
+);
