7f7d6bd2eab64c70156bacc65ee33a54de685f11
Artag-Chris <scristxyz@gmail.com>
2024-09-06 16:59:40 -0500

    redireccionando tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import AppRouter from './router/Router';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 
 const App: React.FC = () => {
   return (
-    <div>
-      
-      {<Header />}
-      {<AppRouter />}
+    <div className="flex">
+      <Header />
+      <Sidebar />
+      <AppRouter />
       <Footer />
     </div>
   );
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Sidebar: React.FC = () => {
+  return (
+    <div className="">
+      <ul className="">
+        <li className=""><a href="/">Home</a></li>
+       
+        <li className=""><a href="/about">About</a></li>
+      
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
